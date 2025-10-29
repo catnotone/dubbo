975c0043fcec658c74ce1ed0938be182c98e1fb6
huang <2822547677@qq.com>
2025-10-29 22:16:13 +0800

    哎

--- common-util/src/test/java/org/huangge/commonutil/CommonUtilApplicationTests.java
+++ common-util/src/test/java/org/huangge/commonutil/CommonUtilApplicationTests.java
@@ -2,12 +2,16 @@ package org.huangge.commonutil;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootTest
+@SpringBootTest(classes = CommonUtilApplicationTests.TestConfig.class)
 class CommonUtilApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Configuration
+    static class TestConfig {
+    }
 }
